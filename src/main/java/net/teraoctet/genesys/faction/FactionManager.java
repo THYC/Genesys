2cf23531a6a722b81d07ab6990d725029ddf72b0
THYC <thyc@teraoctet.net>
2016-04-01 20:49:36 +0200

    Ajout fonction dans FactionManager

--- src/main/java/net/teraoctet/genesys/faction/FactionManager.java
+++ src/main/java/net/teraoctet/genesys/faction/FactionManager.java
@@ -1,8 +1,12 @@
 package net.teraoctet.genesys.faction;
 
+import java.util.ArrayList;
+import java.util.List;
 import net.teraoctet.genesys.player.GPlayer;
+import net.teraoctet.genesys.utils.GData;
 import static net.teraoctet.genesys.utils.GData.getFactions;
 import static net.teraoctet.genesys.utils.GData.getGPlayer;
+import static net.teraoctet.genesys.utils.GData.getPlayers;
 import org.spongepowered.api.entity.living.player.Player;
 
 public class FactionManager {
@@ -54,5 +58,50 @@ public class FactionManager {
             key = getFactions().get(size).getID() + 1;
         }
         return key;
+    }
+    
+    /**
+     * Expulse tous les joueurs d'une faction associé à son ID
+     * @param id_faction 
+     */
+    public void removePlayersToFaction(int id_faction){
+        for (String uuid : getPlayers().keySet()) {
+            if(getPlayers().get(uuid).getID_faction() == id_faction){
+                getPlayers().get(uuid).setID_faction(0);
+                getPlayers().get(uuid).update();
+            }
+        }
+        GData.commit();
+    }
+    
+    /**
+     * Retourne la liste de joueur d'une faction associé à son ID
+     * @param id_faction
+     * @return 
+     */
+    public List getListPlayerFaction(int id_faction){
+        List listPlayer = null;
+        for (String uuid : getPlayers().keySet()) {
+            if(getPlayers().get(uuid).getID_faction() == id_faction){
+                listPlayer.add(getPlayers().get(uuid).getName());
+            }
+        }
+        return listPlayer;
+    }
+    
+    /**
+     * Retourne la liste de joueur d'une faction associé à son ID et son Rank
+     * @param id_faction
+     * @param rank
+     * @return 
+     */
+    public List getListPlayerFaction(int id_faction, int rank){
+        List listPlayer = null;
+        for (String uuid : getPlayers().keySet()) {
+            if(getPlayers().get(uuid).getID_faction() == id_faction && getPlayers().get(uuid).getFactionRank() == rank){
+                listPlayer.add(getPlayers().get(uuid).getName());
+            }
+        }
+        return listPlayer;
     }
 }
